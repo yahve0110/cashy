4969d561a49678988245f6ed29482f43610a2dd3
Ilya <ilya@Ilyas-MacBook-Pro.local>
2025-04-12 17:21:28 +0300

    modal 90 deg

--- src/components/modals/notEnoughEnergyModal/NotEnoughEnergyModal.tsx
+++ src/components/modals/notEnoughEnergyModal/NotEnoughEnergyModal.tsx
@@ -24,90 +24,93 @@ const NotEnoughEnergyModal = ({
   }
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <div className={styles.modalContent}>
-        <div className={styles.modalInner}>
-          <h2 className={styles.title}>Out of Energy</h2>
+    <Modal isOpen={isOpen} onClose={onClose} customStyle>
+      <div className={styles.modalWrapper}>
+        <div className={styles.modalContent}>
+          <div className={styles.modalInner}>
+            <h2 className={styles.title}>Out of Energy</h2>
 
-          <div className={styles.refreshInfo}>
-            <span className={styles.refreshText}>REFRESH IN: </span>
-            <div className={styles.refreshRight}>
-              <img src={alarmIcon} alt="alarmIcon" />
-              <CountdownText targetTime={gameTargetRef.current!} format="hms" />
-            </div>
-          </div>
-
-          <div className={styles.row}>
-            <div className={styles.left}>
-              <span className={styles.description}>Go to Players to send</span>
-              <span className={styles.description}> and receive energy!</span>
-              <button className={styles.button}>Go to Players</button>
+            <div className={styles.refreshInfo}>
+              <span className={styles.refreshText}>REFRESH IN: </span>
+              <div className={styles.refreshRight}>
+                <img src={alarmIcon} alt="alarmIcon" />
+                <CountdownText targetTime={gameTargetRef.current!} format="hms" />
+              </div>
             </div>
 
-            <div className={styles.right}>
-              <span className={styles.modalUpperText}>Join the Club</span>
-              <img className={styles.monemon} src={monemon} alt="monemon" />
-              <div className={styles.perkList}>
-                <div className={styles.perkItem}>
-                  <div className={styles.perkContainer}>
-                    <img
-                      className={styles.lightningIcon}
-                      src={ICONS.lightning}
-                    />
-                    <img className={styles.perkBonus} src={twentyFive} />
-                  </div>
-                  <div className={styles.perkTextContainer}>
-                    <span>25 energy</span>
-                    <span>now</span>
-                  </div>
-                </div>
+            <div className={styles.row}>
+              <div className={styles.left}>
+                <span className={styles.description}>Go to Players to send</span>
+                <span className={styles.description}> and receive energy!</span>
+                <button className={styles.button}>Go to Players</button>
+              </div>
 
-                <div className={styles.perkItem}>
-                  <div className={styles.perkContainer}>
-                    <img
-                      alt={"lightningIcon"}
-                      className={styles.lightningIcon}
-                      src={ICONS.lightning}
-                    />
-                    <img
-                      alt={"perkDoubleIcon"}
-                      className={styles.perkDouble}
-                      src={double}
-                    />
+              <div className={styles.right}>
+                <span className={styles.modalUpperText}>Join the Club</span>
+                <img className={styles.monemon} src={monemon} alt="monemon" />
+                <div className={styles.perkList}>
+                  <div className={styles.perkItem}>
+                    <div className={styles.perkContainer}>
+                      <img
+                        className={styles.lightningIcon}
+                        src={ICONS.lightning}
+                      />
+                      <img className={styles.perkBonus} src={twentyFive} />
+                    </div>
+                    <div className={styles.perkTextContainer}>
+                      <span>25 energy</span>
+                      <span>now</span>
+                    </div>
                   </div>
-                  <div className={styles.perkTextContainer}>
-                    <span>2x daily</span>
-                    <span>energy</span>
-                  </div>
-                </div>
 
-                <div className={styles.perkItem}>
-                  <div className={styles.perkContainer}>
-                    <img
-                      className={styles.silverCoin}
-                      alt={"coinIcon"}
-                      src={silverCoin}
-                    />
-                    <img
-                      className={styles.perkDoubleWhite}
-                      alt={"doubleIcon"}
-                      src={doubleWhite}
-                    />
+                  <div className={styles.perkItem}>
+                    <div className={styles.perkContainer}>
+                      <img
+                        alt={"lightningIcon"}
+                        className={styles.lightningIcon}
+                        src={ICONS.lightning}
+                      />
+                      <img
+                        alt={"perkDoubleIcon"}
+                        className={styles.perkDouble}
+                        src={double}
+                      />
+                    </div>
+                    <div className={styles.perkTextContainer}>
+                      <span>2x daily</span>
+                      <span>energy</span>
+                    </div>
                   </div>
-                  <div className={styles.perkTextContainer}>
-                    <span>x2 monecoin</span>
-                    <span>rewards</span>
+
+                  <div className={styles.perkItem}>
+                    <div className={styles.perkContainer}>
+                      <img
+                        className={styles.silverCoin}
+                        alt={"coinIcon"}
+                        src={silverCoin}
+                      />
+                      <img
+                        className={styles.perkDoubleWhite}
+                        alt={"doubleIcon"}
+                        src={doubleWhite}
+                      />
+                    </div>
+                    <div className={styles.perkTextContainer}>
+                      <span>x2 monecoin</span>
+                      <span>rewards</span>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+          <button className={styles.closeButton} onClick={onClose}>
+          <span className={styles.line1}></span>
+          <span className={styles.line2}></span>
+        </button>
         </div>
+  
       </div>
-      <button className={styles.closeButton} onClick={onClose}>
-        <span className={styles.line1}></span>
-        <span className={styles.line2}></span>
-      </button>
     </Modal>
   );
 };
